1e536bd75faf52d03a6dc026c45a0a1bc231fcc7
Erica Nafziger and Luke Philips <erica@>
2017-01-04 10:42:57 -0800

    create search object and use api to fetch data

--- js/movie-interface.js
+++ js/movie-interface.js
@@ -1,3 +1,20 @@
+var apiKey = require('./../.env').apiKey;
+var Search = require('./../js/movie.js').searchModule;
+
 $(document).ready(function() {
-  console.log("hi!");
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var searchType = $('#searchOne').val();
+    var searchString = $('#searchOneText').val();
+    console.log(searchType+ " - " + searchString);
+    $.get("https://api.themoviedb.org/3/search/"+searchType+"?api_key="+apiKey+"&language=en-US&query="+searchString+"&page=1&include_adult=false").then(function(response) {
+      console.log(response);
+      var userSearch = new Search();
+      userSearch.resultsSet(response);
+      console.log(userSearch);
+    }).fail(function(error){
+      console.log(error);
+    });
+  });
 });
