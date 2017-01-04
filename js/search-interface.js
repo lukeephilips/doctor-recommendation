9dc60f0e710f2ecc591f55bf9ddfddef1f22c674
Erica Nafziger and Luke Philips <erica+luke@>
2017-01-04 14:28:36 -0800

    finds cast of a selected movie

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,32 +1,42 @@
 var apiKey = require('./../.env').apiKey;
 var Search = require('./../js/search.js').searchModule;
-var title = require('./../js/title.js').titleModule;
 
 $(document).ready(function() {
   $('form').submit(function(event) {
     $('#output').html("");
     event.preventDefault();
-
     var searchType = $('#searchOne').val();
-
     var searchString = $('#searchOneText').val();
-
     $.get("https://api.themoviedb.org/3/search/"+searchType+"?api_key="+apiKey+"&language=en-US&query="+searchString+"&page=1&include_adult=false").then(function(response) {
-      debugger;
       if(response.results.length === 0) {
         $('#output').text('Sorry Charlie. Check your spelling.');
       } else {
           var userSearch = new Search();
           userSearch.resultsSet(response);
           console.log(userSearch);
-          if (searchType === title) {
+          if (searchType === "movie") {
             userSearch.titleSet();
+            $('.poster').click(function() {
+              debugger;
+              $.get("https://api.themoviedb.org/3/movie/" + this.id + "/credits?api_key="+ apiKey).then(function(response) {
+                console.log(response);
+                userSearch.cast(response);
+              });
+            });
           } else {
             userSearch.actorSet();
+            $('.image').click(function() {
+
+            });
           }
         }
       }).fail(function(error){
       console.log(error);
     });
   });
+  // click a movie
+
+
+
+
 });
--- js/search.js
+++ js/search.js
@@ -10,18 +10,31 @@ Search.prototype.titleSet = function() {
   this.results.results.forEach(function(item){
     var title = item.title;
     var date = item.release_date;
-    var poster = "<img src='http://image.tmdb.org/t/p/w185/" + item.poster_path + "'>";
+    var id = item.id;
+    var poster = "<img id='" + id + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + item.poster_path + "'>";
     var overview = item.overview;
     $("#output").append("<div class='movie'><h3>" + title + "</h3><h5>" + date + "</h5><p>" + overview + "</p>" + poster + "</div>");
   });
+
 };
 Search.prototype.actorSet = function() {
   this.results.results.forEach(function(item){
     var name = item.name;
     var knownFor = item.known_for[0].title;
-    var image = "<img src='http://image.tmdb.org/t/p/w185/" + item.profile_path + "'>";
+    var image = "<img class='image' src='http://image.tmdb.org/t/p/w185/" + item.profile_path + "'>";
     var id = item.id;
-    $("#output").append("<div class='movie'><h3>" + name + "</h3><h5>Known for: " + knownFor + "</h5><p>" + id + "</p>" + image + "</div>");
+    $("#output").append("<div class='movie'><h3>" + name + "</h3><h5>Known for: " + knownFor + "</h5>" + image + "</div>");
   });
 };
+Search.prototype.cast = function (actors) {
+  debugger;
+  for (i=0; i<6; i++) {
+    console.log(actors[i]);
+    var name = actors.cast[i].name;
+    var character = actors.cast[i].character;
+    var id = actors.cast[i].id;
+    var profile = "<img class='image' src='http://image.tmdb.org/t/p/w185/" + actors.cast[i].profile_path + "'>";
+  }
+};
+
 exports.searchModule = Search;
