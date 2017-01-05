836ca55f9b386fd632ca3b1ec4fadcd7bf2a643e
Erica Nafziger and Luke Philips <erica+luke@>
2017-01-04 16:14:02 -0800

    movie inof on modal

--- js/search-interface.js
+++ js/search-interface.js
@@ -16,13 +16,26 @@ $(document).ready(function() {
           userSearch.resultsSet(response);
           console.log(userSearch);
           if (searchType === "movie") {
-            userSearch.titleSet();
+            userSearch.posterSet();
+
             $('.poster').click(function() {
+              $('#outputDetail').html("");
+              $.get("https://api.themoviedb.org/3/movie/"+this.id+"?api_key="+ apiKey+"&language=en-US").then(function(response) {
+                console.log("response = " + response);
+                userSearch.titleSet(response);
+              });
+
+
+
               $.get("https://api.themoviedb.org/3/movie/" + this.id + "/credits?api_key="+ apiKey).then(function(response) {
                 console.log(response);
                 userSearch.cast(response);
               });
+
               $("#outputDetail").show();
+              $('#outputDetail').click(function() {
+                $('#outputDetail').hide();
+              });
             });
           } else {
             userSearch.actorSet();
--- js/search.js
+++ js/search.js
@@ -6,33 +6,39 @@ Search.prototype.resultsSet = function(array) {
   this.results = array;
 };
 
-Search.prototype.titleSet = function() {
+Search.prototype.posterSet = function() {
   this.results.results.forEach(function(item){
-    var title = item.title;
-    var date = item.release_date;
     var id = item.id;
     var image = item.poster_path;
+    var poster;
     if (image) {
-      var poster = "<img id='" + id + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + item.poster_path + "'>";
+      poster = "<img id='" + id + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + item.poster_path + "'>";
     } else {
-      var poster = "<img id='" + id + "' class='poster' src='http://cdn.topdogtips.com/wp-content/uploads/2015/03/Dog-Nutrition-101-A-Quick-Overview-of-Dog-Feeding-2.jpg'>";
+      poster = "<img id='" + id + "' class='poster' src='http://cdn.topdogtips.com/wp-content/uploads/2015/03/Dog-Nutrition-101-A-Quick-Overview-of-Dog-Feeding-2.jpg'>";
     }
-    var overview = item.overview;
     $("#output").append("<div class='movie'>" + poster + "</div>");
   });
 
 };
+Search.prototype.titleSet = function(movie) {
+  var title = movie.title;
+  var date = movie.release_date;
+  var id = movie.id;
+  var overview = movie.overview;
+  $('#outputDetail').html("<div class='movieInformation'>"+"<h3>"+title+"</h3>"+"</div>");
+  debugger;
+};
+
 Search.prototype.actorSet = function() {
   this.results.results.forEach(function(item){
     var name = item.name;
     var knownFor = item.known_for[0].title;
-    var image = "<img class='image' src='http://image.tmdb.org/t/p/w185/" + item.profile_path + "'>";
     var id = item.id;
+    var image = "<img class='image' src='http://image.tmdb.org/t/p/w185/" + item.profile_path + "'>";
     $("#output").append("<div class='movie'><h3>" + name + "</h3><h5>Known for: " + knownFor + "</h5>" + image + "</div>");
   });
 };
 Search.prototype.cast = function (actors) {
-  $('#outputDetail').html("");
   for (i=0; i<6; i++) {
     console.log(actors[i]);
     var name = actors.cast[i].name;
