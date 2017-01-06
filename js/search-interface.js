f5c3b4ca4be4cca7615602023fffc892a4036196
Luke <luke.e.philips@gmail.com>
2017-01-05 22:53:59 -0800

    initial files and gulp tasks

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,60 +1,10 @@
-var apiKey = require('./../.env').apiKey;
-var Search = require('./../js/search.js').searchModule;
-var Listeners = require('./../js/poster-listener.js');
-var loopingIdModule = require('./../js/headshot-listener.js');
+var getDoctors = require('./../js/search.js').getDoctors;
 
 $(document).ready(function() {
-  $("#outputDetail").hide();
-  $('form').submit(function(event) {
-    $('#output').html("");
-    var loopingId = loopingIdModule.loopingId;
-    console.log("hi"+loopingId);
-    if (loopingId) {
-      $('#'+loopingId).trigger("click");
-    }
-
+  $("form").submit(function(event){
     event.preventDefault();
-    var searchType = $('#searchOne').val();
-    var searchString = $('#searchOneText').val();
-    if (searchString === "") {
-      $('#output').html("<div id='error'><h3>Search for something.</h3></div>");
-    }
-// poster API call
-    $.get("https://api.themoviedb.org/3/search/"+searchType+"?api_key="+apiKey+"&language=en-US&query="+searchString+"&page=1&include_adult=false").then(function(response) {
-      if(response.results.length === 0) {
-        $('#output').html("<div id='error'><h3>Sorry Charlie. Check your spelling.</h3></div>");
-      } else {
-          var userSearch = new Search();
-          userSearch.resultsSet(response);
-          if (searchType === "movie") {
-            userSearch.posterSet();
-// 1st level click on movie poster to open movie details
-            $('.poster').click(function() {
-              Listeners.PosterClickListener(userSearch,response, this.id);
-            });
-          } else {
-            userSearch.actorSet(response);
-            $('.image').click(function() {
-              $.get("https://api.themoviedb.org/3/person/" + this.id + "/movie_credits?api_key="+ apiKey +"&page=1&language=en-US").then(function(newResponse) {
-                userSearch.resultsSet(newResponse);
-                console.log(userSearch);
-                userSearch.actorToMovie();
-                $('.poster').click(function() {
-                  $('#searchOne').val("movie");
-                  Listeners.PosterClickListener(userSearch,response, this.id);
-                });
-              });
-              // loopingIdModule.headshotClickListener(response, this.id);
-            });
-          }
-        }
-      }).fail(function(error){
-      console.log(error);
-    });
+    var symptom = $('#symptom').val();
+debugger;
+    getDoctors(symptom);
   });
-  // click a movie
-
-
-
-
 });
--- js/search.js
+++ js/search.js
@@ -1,103 +1,25 @@
+var apiKey = require('./../.env').apiKey;
 
-function Search() {
-  this.results = [];
-}
+exports.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+      result.data.forEach(function(doctor) {
+        var fname = doctor.profile.first_name;
+        var lname = doctor.profile.last_name;
+        var pic = doctor.profile.image_url;
+        var bio = doctor.profile.bio;
 
-Search.prototype.resultsSet = function(array) {
-  this.results = array;
+        var specialties = []
+        doctor.specialties.forEach(function(specialty){
+          specialties.push(specialty.name)
+        });
+$('#output').append("<div><h2>"+fname+" "+lname+"</h2> <p class='bio'>"+bio+"</p><img class='headshot' src='"+pic+"'></div>")
+      });
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
 };
 
-Search.prototype.posterSet = function() {
-  this.results.results.forEach(function(item){
-    var id = item.id;
-    var image = item.poster_path;
-    var poster;
-    if (image) {
-      poster = "<img id='" + id + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + image + "'>";
-      $("#output").append("<div class='movie'>" + poster + "</div>");
-    }
-  });
-};
-Search.prototype.actorToMovie = function() {
-  $('#output').html("");
-  debugger;
-  this.results.cast.forEach(function(item){
-    var id = item.id;
-    var image = item.poster_path;
-    var poster;
-    if (image) {
-      poster = "<img id='" + id + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + image + "'>";
-      $("#output").append("<div class='movie'>" + poster + "</div>");
-    }
-  });
-};
-
-Search.prototype.inputSet = function(movie) {
-  var title = movie.title;
-  var id = movie.id;
-  $('input').val(title);
-  $('form').trigger('submit');
-};
-
-Search.prototype.titleSet = function(movie) {
-  var title = movie.title;
-  var date = movie.release_date;
-  var id = movie.id;
-  var overview = movie.overview;
-  var budget = movie.budget;
-
-  var genres = [];
-  genres.push(movie.genres[0].name, movie.genres[1].name);
-  genres = genres.join(" and ");
-
-  var backdrop;
-  var backdropImage = movie.backdrop_path;
-  if (backdropImage) {
-    backdrop = "http://image.tmdb.org/t/p/original/" + backdropImage;
-  }
-  $('#outputDetail .movieDetails').append("<h2>"+title+"</h2><h6>Release date: "+date+"</h6><h6>Genre: "+ genres+"</h6><p>"+overview+"</p>");
-  $('#outputDetail').css('background-image', 'url('+ backdrop +')');
-
-};
-
-Search.prototype.actorSet = function() {
-  this.results.results.forEach(function(item){
-    var name = item.name;
-    // var knownFor = item.known_for[0].title;
-    var actorId = item.id;
-    var photo = item.profile_path;
-    var image;
-    if (photo) {
-      image = "<img class='image' id='"+actorId+"' src='http://image.tmdb.org/t/p/w185/" + item.profile_path + "'>";
-      $("#output").append("<div class='headshot'><h3>" + name + "</h3>" + image + "</div>");
-    }
-  });
-};
-
-Search.prototype.cast = function (actors) {
-  for (i=0; i<6; i++) {
-    var name = actors.cast[i].name;
-    var character = actors.cast[i].character;
-    var id = actors.cast[i].id;
-    var profileImage = actors.cast[i].profile_path;
-    var profile;
-    if (profileImage) {
-      profile = "<img class='image' id='"+id+"' src='http://image.tmdb.org/t/p/w185/" + actors.cast[i].profile_path + "'>";
-
-      $("#outputDetail .actors").append("<div class='actor'><h3>" + name + "</h3><h5>Character: " + character + "</h5>" + profile + "</div>");
-    }
-  }
-};
-Search.prototype.filmography = function(films) {
-  films.cast.forEach(function(film) {
-    var filmId = film.id;
-    var poster;
-    var image = film.poster_path;
-    if (image) {
-      poster = "<img id='" + filmId + "' class='poster' src='http://image.tmdb.org/t/p/w185/" + image + "'>";
-      $("#output").append("<div class='movie'>" + poster + "</div>");
-    }
-  });
-};
-
-exports.searchModule = Search;
+// exports.getDoctorsModule = getDoctors;
