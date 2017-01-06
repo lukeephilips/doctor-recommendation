604bda3621b16b8dfc3341322c36a0270c8337f1
Luke <luke.e.philips@gmail.com>
2017-01-06 10:27:56 -0800

    everything working and looking snazzyish

--- js/search.js
+++ js/search.js
@@ -2,11 +2,13 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getDoctors = function(medicalIssue) {
   $('#output').html("");
+
+  $('#output').append("<h5>You searched for: "+medicalIssue+"</h5>");
   var apiCall = $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
-     console.log(apiKey);
-     if (!response) {
-       $('#output').html("<h2 id='error'>No results found</h2>");
+     console.log(response);
+     if (response.data.length === 0) {
+       $('#output').append("<h2 id='error'>No results found</h2>");
      }
      else {
        response.data.forEach(function(doctor) {
@@ -19,7 +21,7 @@ exports.getDoctors = function(medicalIssue) {
          doctor.specialties.forEach(function(specialty){
            specialties.push(specialty.name);
          });
-         $('#output').append("<div><p>You searched for: "+medicalIssue+"</p></div><div><h2>"+fname+" "+lname+"</h2> <p class='bio'>"+bio+"</p><img class='headshot' src='"+pic+"'></div>");
+         $('#output').append("<div class='row'><h2>"+fname+" "+lname+"</h2> <div class='col-sm-2'><img class='headshot' src='"+pic+"'></div><div class='col-sm-10'><p class='bio'>"+bio+"</p></div></div>");
        });
      }
 })
