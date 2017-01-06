4fe6166d30309497e88a34cdc4fb0a22b93c7436
Luke <luke.e.philips@gmail.com>
2017-01-06 08:58:53 -0800

    add API call

--- js/search-interface.js
+++ js/search-interface.js
@@ -5,6 +5,23 @@ $(document).ready(function() {
     event.preventDefault();
     var symptom = $('#symptom').val();
 debugger;
-    getDoctors(symptom);
+    var result = getDoctors(symptom);
+    if (!result) {
+      $('#output').html("<h2 id='error'>No results found</h2>");
+    }
+    else {
+      result.data.forEach(function(doctor) {
+        var fname = doctor.profile.first_name;
+        var lname = doctor.profile.last_name;
+        var pic = doctor.profile.image_url;
+        var bio = doctor.profile.bio;
+
+        var specialties = []
+        doctor.specialties.forEach(function(specialty){
+          specialties.push(specialty.name)
+        });
+  $('#output').append("<div><h2>"+fname+" "+lname+"</h2> <p class='bio'>"+bio+"</p><img class='headshot' src='"+pic+"'></div>")
+      });
+    }
   });
 });
--- js/search.js
+++ js/search.js
@@ -4,18 +4,6 @@ exports.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
-      result.data.forEach(function(doctor) {
-        var fname = doctor.profile.first_name;
-        var lname = doctor.profile.last_name;
-        var pic = doctor.profile.image_url;
-        var bio = doctor.profile.bio;
-
-        var specialties = []
-        doctor.specialties.forEach(function(specialty){
-          specialties.push(specialty.name)
-        });
-$('#output').append("<div><h2>"+fname+" "+lname+"</h2> <p class='bio'>"+bio+"</p><img class='headshot' src='"+pic+"'></div>")
-      });
     })
    .fail(function(error){
       console.log("fail");
